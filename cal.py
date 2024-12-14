e00be9eacf9579d20ccd2726e13018da418751e1
Hephaestus0405 <rahul405.bhardwaj@gmail.com>
2024-12-14 18:57:20 +0530

    Initial commit

---
+++ cal.py
@@ -0,0 +1,34 @@
+# Level One
+
+# def input_val():
+#     for i in range(2):
+#         a = int(input(f"Enter the value one:"))
+#         b = int(input(f"Enter the value two:"))
+#     result = input(f"Enter the operation , you want to perform:")
+#     if result == "mul":
+#         res = a*b
+#         print(res)
+#     elif result == "sub":
+#         res = a-b
+#         print(res)
+#     elif result == "add":
+#         res = a+b
+#         print(res)
+#     elif result == "div":
+#         res = a/b
+#         print(round(res))
+#     else:
+#         print(f"RE-TRY AGAIN BETTER LUCK NEXT TIME!!!")
+
+
+# input_val()
+
+
+# Level Two
+
+def input_val():
+    print("Let's do some Calculations!!!")
+
+    a = int(input(f"Enter the value one:"))
+    b = int(input("Enter the value two:"))
+    
